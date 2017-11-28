0e022e537c824702511e0a7f0f3ca48b631af3e0
Mariusz Sioch <mariusz.sioch@o2.pl>
2017-11-28 17:04:26 +0100

    Add js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var a = prompt('Enter a');
+	b = prompt('Enter b');
+    value = (a * b) - (2 * a * b) + (b * b);
+console.log(value);
+alert(value);
+if (value > 0) {
+	console.log('Liczba dodatnia');
+}
+else if (value < 0) {
+	console.log('Liczba ujemna');
+}
+else (value = 0) {
+	console.log('Liczba równa się 0');
+};
+
