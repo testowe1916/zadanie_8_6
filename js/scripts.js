d637899ed03bd886d479523678c7ab3dd0c0e9e1
Mariusz Sioch <mariusz.sioch@o2.pl>
2017-12-03 11:47:03 +0100

    Mod js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
-var a = prompt('Enter a');
-	b = prompt('Enter b');
+var a = prompt('Enter a'),
+	b = prompt('Enter b'),
     value = (a * b) - (2 * a * b) + (b * b);
 console.log(value);
 alert(value);
 if (value > 0) {
 	console.log('Liczba dodatnia');
+	alert('dodatnia')
 }
 else if (value < 0) {
 	console.log('Liczba ujemna');
+	alert('minus')
 }
-else (value = 0) {
+else if (value == 0) {
 	console.log('Liczba równa się 0');
+	alert('0')
 };
 
